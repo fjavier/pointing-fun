dd8fa7c6e113609bd7ba49b24ad458c69b673980
Francisco Briceño <fjavier.cgam@gmail.com>
2021-08-30 21:43:54 -0600

    Mi primer commit
    Este es un ejemplo para una aplicaciòn de Pointing, para planeaciòn con metodologias agiles.

---
+++ src/main/java/com/example/pointingfun/PointingFunApplication.java
@@ -0,0 +1,13 @@
+package com.example.pointingfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointingFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointingFunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pointingfun/PointingFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pointingfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointingFunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
